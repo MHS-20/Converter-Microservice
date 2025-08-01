d55aeb9121fb6743327b09e4b4d8170dba26f417
MHS <muhamad.salman2001@gmail.com>
2025-08-01 17:40:43 +0200

    feat: added utild to gateway service

--- auth/server.py
+++ auth/server.py
@@ -35,7 +35,7 @@ def login():
     else: 
         return "invalid credentials", 401
 
-@server.route("/validate", method=["POST"])
+@server.route("/validate", methods=["POST"])
 def validate(): 
     encoded_jwt = request.headers["Authorization"]
     if not encoded_jwt:
